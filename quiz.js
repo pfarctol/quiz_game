c604551d12e16833dd1b6895695449aca029c258
pfarctol <142282081+pfarctol@users.noreply.github.com>
2023-09-18 21:43:29 -0600

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,39 @@
+var
+var question1 = document.querySelector
+var question2 = 
+
+
+function() {
+    const questions = [
+        {
+        question: "q1",
+        answers: {
+            a: "1",
+            b: "2",
+            c: "3"
+        },
+        correctAnswer: ""
+        },
+        {
+        question: "q1",
+        answers: {
+            a: "1",
+            b: "2",
+            c: "3"
+        },
+        correctAnswer: ""
+        },
+        {
+            question: "q1",
+        answers: {
+            a: "1",
+            b: "2",
+            c: "3"
+        },
+        correctAnswer: ""
+        }
+    ];
+}
+
+
+
